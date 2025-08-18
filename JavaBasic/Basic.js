81103f4fd6d8273e2d0d1f88d22000617060e982
shashi0505 <61519339+shashi0505@users.noreply.github.com>
2025-08-18 16:44:38 +0530

    first commit

---
+++ JavaBasic/Basic.js
@@ -0,0 +1,9 @@
+
+console.log("Hello World")
+const accountId=1234;
+let accountName="Shashi"
+var accountEmail="Shashi@hhah"
+AccountCity="Jaipur"
+
+console.log(accountId)
+console.table([accountEmail,accountName,AccountCity])
---
+++ JavaBasic/DataTypes.js
@@ -0,0 +1,18 @@
+
+/*
+
+Null is object in javascript
+undefined is undefined 
+when we did not defined any values then it will be undefined 
+
+*/
+let name="Shashi"
+let state;
+
+//tyeof is the function whichh will be gives you the type of data tpes in javascript
+console.log(typeof "Shashi")
+//object wull be print
+console.log(typeof null)
+//undefined will be print 
+console.log(state)
+
